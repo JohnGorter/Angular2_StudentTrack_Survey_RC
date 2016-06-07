235cdb56d543c8f9b18455d999960680a60ec733
John Gorter <John.Gorter@gmail.com>
2016-06-07 13:02:30 +0200

    Lab 2. TypeScript_Starter

--- app/app.ts
+++ app/app.ts
@@ -1,18 +1,48 @@
 import { Component } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
-
+import {Student, StudentTrack} from './models/student';
 
 // create a class with annotations..
-@Component({
-	selector: 'studenttrack-survey',
+@Component({	
+	selector: 'student-track-survey',
 	template: `
-	 <h1>{{title}}</h1>
-	`
+	<div class="studentTrack">
+	   <h1 class="default-primary-color text-primary-color">Student track {{studentTrack.name}} (<span inner-text="getCount()"></span> attendees)</h1>
+	   <div class="student" *ngFor="#student of studentTrack.getStudents()">
+	     {{ student.firstname }} {{ student.lastname }} {{ student.school }}
+	   </div>
+	 </div>
+	`,
+	styles:[`
+	 .student { padding:15px; }
+	 .studentTrack { border:1px solid black;margin:5px;padding:0px; }
+	 .studentTrack h1 { margin:0px;padding:15px;}
+	`]
 })
-class SurveyApplication {
-	public title = 'Studenttrack surveys';
-}
+export class SurveyApplication {	
+	// ADD FIELD FOR THE STUDENTTRACK
+	// your code here...
+	
+	
+	constructor (){
+		// ADD CODE HERE TO INSTANTIATE A NEW STUDENTTRACK
+		// AND ADD SOME STUDENTS TO THE STUDENTTRACK
+		// your code here...
+	   
+	   // REPLACE THIS ANONYMOUS FUNCTION WITH A PHAT ARROW ONE
+	   window.setInterval(function() {
+		   // RANDOMLY ADD NEW ATTENDEES TO THE STUDENTTRACK
+		   // your code here...
+		 	
+	   }, 2000);
+	}
+	
+	getCount(){
+		// ADD CODE THAT RETURNS THE NUMBER OF STUDENTS FROM THE STUDENTTRACK
+		// your code here...
 
+	}
+}
 
 // bootstrap our application..
 bootstrap(SurveyApplication); 
