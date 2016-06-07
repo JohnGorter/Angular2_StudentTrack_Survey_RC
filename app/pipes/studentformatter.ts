211105b8e66818e18ac71003e9c1276d5be4ecb7
John Gorter <John.Gorter@gmail.com>
2016-06-07 15:41:47 +0200

    Lab 4. Pipes_Starter

---
+++ app/pipes/studentformatter.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform  } from '@angular/core';
+// ?code? //
+
+@Pipe({
+	// ?code? //
+    name:''
+})
+export class StudentFormatter implements PipeTransform{
+	 // ?code? //
+}
